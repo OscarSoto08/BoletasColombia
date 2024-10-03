62ce388bc4d92ae2ab3e1e96b4810d1f6276c3ae
Oscar Soto <oscaralejandrosoto9@gmail.com>
2024-10-03 01:17:28 -0500

    interfaz dark

--- Encabezado.php
+++ Encabezado.php
@@ -1,24 +1,29 @@
-<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img id="logo" src="img/icono.png" alt="Logo del negocio boletas colombia">Boletas Colombia
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mb-2 mb-lg-0">
-                <li class="nav-item fs-5">
-                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                </li>
-                <li class="nav-item fs-5">
-                    <a class="nav-link" href="#">Contáctanos</a>
-                </li>
-                <li class="nav-item fs-5">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-            </ul>
-            <span class="navbar-brand ms-auto">Iniciar Sesión</span>
-        </div>
+<div class="header">
+        <h1>Boletas Colombia</h1>
     </div>
-</nav>
+    <nav class="navbar navbar-expand-lg" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img id="logo" src="img/icono.png" alt="Logo del negocio Boletas Colombia" class="me-2"> Boletas Colombia
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contáctanos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+                <span class="navbar-text ms-auto">
+                    <a class="btn btn-light" href="#" role="button">Iniciar Sesión</a>
+                </span>
+            </div>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -1,71 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boletas Colombia</title>
-    
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="css/style.css">
+     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php require 'Encabezado.php';?>
-    <!-- Barra de Navegación -->
-    <nav class="navbar navbar-expand-lg navbar-dark mb-4">
-        <a class="navbar-brand" href="#">Boletas Colombia</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Ingresar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contacto</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php 
+        require 'Encabezado.php';
+        require 'Carrusel.php';
+    ?>
 
-
-    <h2 class="text-center mb-4">TOP Eventos</h2>
-    
-    <!-- Carrusel -->
-    <div id="carrusel" class="carousel slide">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <p class="text-center mt-3 fs-3">Destacado 1</p>
-                <img src="img/icono.png" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <p class="text-center mt-3 fs-3">Destacado 2</p>
-                <img src="img/icono.png" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <p class="text-center mt-3 fs-3">Destacado 3</p>
-                <img src="img/icono.png" class="d-block w-100" alt="...">
-            </div>
-        </div>
-
-        <button class="carousel-control-prev" type="button" data-bs-target="#carrusel" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carrusel" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-
-    <!-- Contenedor Principal -->
-    <div class="container mt-4">
         <h2 class="text-center mb-4">Eventos disponibles</h2>
         <div class="row">
             <?php 
@@ -74,22 +26,14 @@
                 $ArrayEventos = $evento->consultarTodos();
                 foreach ($ArrayEventos as $eventoAct) {
             ?>
-                <div class="col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <img src="https://i.ibb.co/C2Kbgx6/icono.png" class="card-img-top" alt="Producto 1">
-                        <div class="card-body d-flex align-center flex-column">
-                            <h5 class="card-title text-center">
-                                <?php echo $eventoAct -> getNombre(); ?>
-                            </h5>
-                            <h6 class="mb-3 text-center">
-                                <i><?php echo $eventoAct -> getdireccion();?></i>
-                            </h6>
-                            <p class="card-text">Fecha: 
-                                <?php echo $eventoAct -> getFecha(); ?>
-                            </p>
-                            <p class="card-text">Aforo: 
-                                <?php echo $eventoAct -> getAforo(); ?>
-                            </p>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://i.ibb.co/C2Kbgx6/icono.png" class="card-img-top" alt="<?php echo $eventoAct->getNombre(); ?>">
+                        <div class="card-body d-flex flex-column align-items-center">
+                            <h5 class="card-title text-center"><?php echo '<h2>' . $eventoAct->getNombre() . '</h2>'; ?></h5>
+                            <h6 class="mb-3 text-center"><i><?php echo $eventoAct->getdireccion(); ?></i></h6>
+                            <p class="card-text">Fecha: <?php echo $eventoAct->getFecha(); ?></p>
+                            <p class="card-text">Aforo: <?php echo $eventoAct->getAforo(); ?></p>
                             <button class="btn btn-primary">Más detalles</button>
                         </div>
                     </div>
@@ -99,5 +43,9 @@
             ?>
         </div>
     </div>
+
+    <footer>
+        <p>&copy; 2024 Boletas Colombia. Todos los derechos reservados.</p>
+    </footer>
 </body>
 </html>
