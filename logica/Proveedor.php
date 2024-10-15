3c45fa74515998ef6358554dad1442252ad59570
CamiloGuasca <papeldormido@gmail.com>
2024-10-15 17:07:47 -0500

    Sesión del proveedor

--- logica/Proveedor.php
+++ logica/Proveedor.php
@@ -19,6 +19,18 @@ class Proveedor extends Persona{
             return true;
         }
     }
-    
+
+    public function consultar(){
+        $conexion = new Conexion();
+        $conexion -> abrirConexion();
+        $proveedorDAO = new ProveedorDAO($this -> idPersona);
+        $conexion -> ejecutarConsulta($proveedorDAO -> consultar());
+        $registro = $conexion -> siguienteRegistro();
+        $this -> nombre = $registro[0];
+        $this -> apellido = $registro[1];
+        $this -> correo = $registro[2];
+        $this -> telefono = $registro[3];
+        $this -> direccion = $registro[4];
+    }
 }
 ?>
